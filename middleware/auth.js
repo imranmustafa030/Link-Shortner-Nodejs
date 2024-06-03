3c0f8624f27a73d0aed798b4bdce1e81358cdf18
imranmustafa031 <imran942mustafa@gmail.com>
2024-06-03 16:25:23 +0500

    convert JWT Cookies -> Hearder

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,13 @@
 const { getUser } = require("../service/auth");
 
 const restrictToLoggedinUserOnly = async (req, res, next) => {
-  const userUid = req.cookies?.uid;
+  const userUid = req.headers["authorization"];
 
   if (!userUid) return res.redirect("/login");
 
-  const user = getUser(userUid);
+  const token = userUid.split("Bearer ")[1];
+
+  const user = getUser(token);
   if (!user) return res.redirect("/login");
 
   req.user = user;
@@ -13,8 +15,10 @@ const restrictToLoggedinUserOnly = async (req, res, next) => {
 };
 
 const checkAuth = async (req, res, next) => {
-  const userUid = req.cookies?.uid;
-  const user = getUser(userUid);
+  const userUid = req.headers["authorization"];
+  const token = userUid.split("Bearer ")[1];
+
+  const user = getUser(token);
   req.user = user;
   next();
 };
